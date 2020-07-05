691d80116b994b6a1c1de4144fec447020c9bda3
Daniel A.A. Pelsmaeker <647530+Virtlink@users.noreply.github.com>
2020-07-05 02:02:33 +0200

    Add BinTray publish

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,15 @@
 import org.jetbrains.dokka.gradle.DokkaTask
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import com.jfrog.bintray.gradle.BintrayPlugin
+import com.jfrog.bintray.gradle.BintrayExtension
+import java.util.Date
 
 plugins {
     java
     `maven-publish`
     kotlin("jvm") version "1.3.72"
     id("org.jetbrains.dokka") version "0.10.1"
+    id("com.jfrog.bintray") version "1.8.5"
 }
 
 repositories {
@@ -61,8 +65,11 @@ val sourcesJar by tasks.creating(Jar::class) {
     from(sourceSets.getByName("main").allSource)
 }
 
-val pomDesc: String by project
+val githubRepo: String by project
+val githubReadme: String by project
+
 val pomUrl: String by project
+val pomIssueUrl: String by project
 val pomLicenseName: String by project
 val pomLicenseUrl: String by project
 val pomLicenseDist: String by project
@@ -82,7 +89,7 @@ publishing {
             pom.withXml {
                 asNode().apply {
                     appendNode("name", rootProject.name)
-                    appendNode("description", pomDesc)
+                    appendNode("description", project.description)
                     appendNode("url", pomUrl)
                     appendNode("licenses").appendNode("license").apply {
                         appendNode("name", pomLicenseName)
@@ -110,6 +117,44 @@ publishing {
             credentials {
                 username = System.getenv("GITHUB_ACTOR")
                 password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
+}
+
+bintray {
+    user = project.findProperty("bintrayUser").toString()
+    key = project.findProperty("bintrayKey").toString()
+    setPublications("lib")
+    publish = true
+
+    pkg.apply {
+        name = project.name
+        desc = project.description
+        websiteUrl = pomUrl
+        issueTrackerUrl = pomIssueUrl
+        vcsUrl = pomUrl + ".git"
+        githubRepo = githubRepo
+        setLicenses("Apache-2.0")
+        repo = project.findProperty("bintrayRepo").toString()
+        publicDownloadNumbers = true
+        setLabels("kotlin", "disjoint", "map", "set", "collection")
+
+        githubReleaseNotesFile = githubReadme
+
+        version.apply {
+            name = project.version.toString()
+            desc = project.description
+            released = Date().toString()
+            vcsTag = project.version.toString()
+            gpg.apply {
+                sign = true
+                passphrase = project.findProperty("gpgPassphrase").toString()
+            }
+            mavenCentralSync.apply {
+                sync = true
+                user = project.findProperty("sonatypeUsername").toString()
+                password = project.findProperty("sonatypePassword").toString()
             }
         }
     }
