e74704cf32fa93a4088ffc2afd7a0fb56f8c0f8e
Daniel A. A. Pelsmaeker <developer@pelsmaeker.net>
2025-04-12 14:28:09 +0200

    Make project build again

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.dokka.gradle.DokkaTask
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-import com.jfrog.bintray.gradle.BintrayPlugin
-import com.jfrog.bintray.gradle.BintrayExtension
+//import com.jfrog.bintray.gradle.BintrayPlugin
+//import com.jfrog.bintray.gradle.BintrayExtension
 import java.util.Date
 
 plugins {
@@ -9,7 +9,6 @@ plugins {
     `maven-publish`
     kotlin("jvm") version "1.5.31"
     id("org.jetbrains.dokka") version "0.10.1"
-    id("com.jfrog.bintray") version "1.8.5"
 }
 
 repositories {
@@ -65,97 +64,97 @@ val sourcesJar by tasks.creating(Jar::class) {
     from(sourceSets.getByName("main").allSource)
 }
 
-val githubRepo: String by project
-val githubReadme: String by project
+//val githubRepo: String by project
+//val githubReadme: String by project
+//
+//val pomUrl: String by project
+//val pomIssueUrl: String by project
+//val pomLicenseName: String by project
+//val pomLicenseUrl: String by project
+//val pomLicenseDist: String by project
+//val pomDeveloperId: String by project
+//val pomDeveloperName: String by project
+//val pomScmUrl: String by project
+//val pomScmConnection: String by project
+//val pomScmDevConnection: String by project
 
-val pomUrl: String by project
-val pomIssueUrl: String by project
-val pomLicenseName: String by project
-val pomLicenseUrl: String by project
-val pomLicenseDist: String by project
-val pomDeveloperId: String by project
-val pomDeveloperName: String by project
-val pomScmUrl: String by project
-val pomScmConnection: String by project
-val pomScmDevConnection: String by project
+//publishing {
+//    publications {
+//        create<MavenPublication>("lib") {
+//            from(components["java"])
+//            artifact(dokkaJar)
+//            artifact(sourcesJar)
+//
+//            pom.withXml {
+//                asNode().apply {
+//                    appendNode("name", rootProject.name)
+//                    appendNode("description", project.description)
+//                    appendNode("url", pomUrl)
+//                    appendNode("licenses").appendNode("license").apply {
+//                        appendNode("name", pomLicenseName)
+//                        appendNode("url", pomLicenseUrl)
+//                        appendNode("distribution", pomLicenseDist)
+//                    }
+//                    appendNode("developers").appendNode("developer").apply {
+//                        appendNode("id", pomDeveloperId)
+//                        appendNode("name", pomDeveloperName)
+//                    }
+//                    appendNode("scm").apply {
+//                        appendNode("url", pomScmUrl)
+//                        appendNode("connection", pomScmConnection)
+//                        appendNode("developerConnection", pomScmDevConnection)
+//                    }
+//                }
+//            }
+//        }
+//    }
+//
+//    repositories {
+//        maven {
+//            name = "GitHubPackages"
+//            url = uri("https://maven.pkg.github.com/virtlink/disjoint-map")
+//            credentials {
+//                username = System.getenv("GITHUB_ACTOR")
+//                password = System.getenv("GITHUB_TOKEN")
+//            }
+//        }
+//    }
+//}
 
-publishing {
-    publications {
-        create<MavenPublication>("lib") {
-            from(components["java"])
-            artifact(dokkaJar)
-            artifact(sourcesJar)
-
-            pom.withXml {
-                asNode().apply {
-                    appendNode("name", rootProject.name)
-                    appendNode("description", project.description)
-                    appendNode("url", pomUrl)
-                    appendNode("licenses").appendNode("license").apply {
-                        appendNode("name", pomLicenseName)
-                        appendNode("url", pomLicenseUrl)
-                        appendNode("distribution", pomLicenseDist)
-                    }
-                    appendNode("developers").appendNode("developer").apply {
-                        appendNode("id", pomDeveloperId)
-                        appendNode("name", pomDeveloperName)
-                    }
-                    appendNode("scm").apply {
-                        appendNode("url", pomScmUrl)
-                        appendNode("connection", pomScmConnection)
-                        appendNode("developerConnection", pomScmDevConnection)
-                    }
-                }
-            }
-        }
-    }
-
-    repositories {
-        maven {
-            name = "GitHubPackages"
-            url = uri("https://maven.pkg.github.com/virtlink/disjoint-map")
-            credentials {
-                username = System.getenv("GITHUB_ACTOR")
-                password = System.getenv("GITHUB_TOKEN")
-            }
-        }
-    }
-}
-
-bintray {
-    user = project.findProperty("bintrayUser").toString()
-    key = project.findProperty("bintrayKey").toString()
-    setPublications("lib")
-    publish = true
-
-    pkg.apply {
-        name = project.name
-        desc = project.description
-        websiteUrl = pomUrl
-        issueTrackerUrl = pomIssueUrl
-        vcsUrl = pomUrl + ".git"
-        githubRepo = githubRepo
-        setLicenses("Apache-2.0")
-        repo = project.findProperty("bintrayRepo").toString()
-        publicDownloadNumbers = true
-        setLabels("kotlin", "disjoint", "map", "set", "collection")
-
-        githubReleaseNotesFile = githubReadme
-
-        version.apply {
-            name = project.version.toString()
-            desc = project.description
-            released = Date().toString()
-            vcsTag = project.version.toString()
-            gpg.apply {
-                sign = true
-                passphrase = project.findProperty("gpgPassphrase").toString()
-            }
-            mavenCentralSync.apply {
-                sync = true
-                user = project.findProperty("sonatypeUsername").toString()
-                password = project.findProperty("sonatypePassword").toString()
-            }
-        }
-    }
-}
+//bintray {
+//    user = project.findProperty("bintrayUser").toString()
+//    key = project.findProperty("bintrayKey").toString()
+//    setPublications("lib")
+//    publish = true
+//
+//    pkg.apply {
+//        name = project.name
+//        desc = project.description
+//        websiteUrl = pomUrl
+//        issueTrackerUrl = pomIssueUrl
+//        vcsUrl = pomUrl + ".git"
+//        githubRepo = githubRepo
+//        setLicenses("Apache-2.0")
+//        repo = project.findProperty("bintrayRepo").toString()
+//        publicDownloadNumbers = true
+//        setLabels("kotlin", "disjoint", "map", "set", "collection")
+//
+//        githubReleaseNotesFile = githubReadme
+//
+//        version.apply {
+//            name = project.version.toString()
+//            desc = project.description
+//            released = Date().toString()
+//            vcsTag = project.version.toString()
+//            gpg.apply {
+//                sign = true
+//                passphrase = project.findProperty("gpgPassphrase").toString()
+//            }
+//            mavenCentralSync.apply {
+//                sync = true
+//                user = project.findProperty("sonatypeUsername").toString()
+//                password = project.findProperty("sonatypePassword").toString()
+//            }
+//        }
+//    }
+//}
